927f2d2ead15f3d5a4244b235e30c0d34b1ec986
Nina <nina.marjanovic@gmail.com>
2016-12-20 16:11:43 +0100

    Controlling RC car via keyboard

--- AutonomniAutomobil/rc_car/car_arduino/car_arduino.ino
+++ AutonomniAutomobil/rc_car/car_arduino/car_arduino.ino
@@ -1,19 +1,44 @@
-const int ledPin = 13;
+const int backPin = 7;
+const int frontPin = 8;
+const int leftPin = 12;
+const int rightPin = 13;
+
 int incomingByte;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(frontPin, OUTPUT);
+  pinMode(backPin, OUTPUT);
+  pinMode(leftPin, OUTPUT);
+  pinMode(rightPin, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
-    if (incomingByte == 'H') {
-      digitalWrite(ledPin, HIGH);
+    if (incomingByte == '0') {
+      digitalWrite(frontPin, HIGH);
     } 
-    if (incomingByte == 'L') {
-      digitalWrite(ledPin, LOW);
+    if (incomingByte == '1') {
+      digitalWrite(frontPin, LOW);
+    }
+    if (incomingByte == '2'){
+      digitalWrite(backPin, HIGH);  
+    }
+    if (incomingByte == '3'){
+      digitalWrite(backPin, LOW);  
+    }
+    if (incomingByte == '4'){
+      digitalWrite(leftPin, HIGH);  
+    }
+    if (incomingByte == '5'){
+      digitalWrite(leftPin, LOW);  
+    }
+    if (incomingByte == '6'){
+      digitalWrite(rightPin, HIGH);  
+    }
+    if (incomingByte == '7'){
+      digitalWrite(rightPin, LOW);  
     }
   }
 }
