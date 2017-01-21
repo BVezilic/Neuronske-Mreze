8fc6e6c675b545634e7e6c2b80b050ecef9f8d3b
rasajski <vwm0w0mwv@gmail.com>
2017-01-21 17:43:16 +0100

    Izmena na detekciji i slika za orb

--- AutonomniAutomobil/object_detection/sign_detection.py
+++ AutonomniAutomobil/object_detection/sign_detection.py
@@ -1,15 +1,10 @@
 import cv2
-from keras.applications.resnet50 import ResNet50
-import keras.preprocessing.image as kp
-from keras.applications.resnet50 import preprocess_input, decode_predictions
-import numpy as np
 
-model = ResNet50(weights='imagenet')
 
 def detect_cars(image):
     # constants
     IMAGE_SIZE = 200.0
-    MATCH_THRESHOLD = 3
+    MATCH_THRESHOLD = 1
 
     #cascade_url = 'frontal_stop_sign_cascade.xml'
     cascade_url = 'cars.xml'
@@ -26,13 +21,13 @@ def detect_cars(image):
         minNeighbors=1
     )
     #print signs
-    # orb i feature matcher
-    #orb = cv2.ORB()
+    #orb i feature matcher
+    orb = cv2.ORB()
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     # kljucne karakteristike za znake koje sluze za odbacivanje lose prepoznatih delova
-    #roadsign = cv2.imread('examples/stop5.jpg', 0)
-    #kp_r, des_r = orb.detectAndCompute(roadsign, None)
+    roadsign = cv2.imread('orb_examples/orb_feature.jpg', 0)
+    kp_r, des_r = orb.detectAndCompute(roadsign, None)
 
     # prolazak kroz sve znake
     for (x, y, w, h) in signs:
@@ -43,22 +38,19 @@ def detect_cars(image):
         obj = cv2.resize(obj, (int(IMAGE_SIZE), int(obj.shape[0] * ratio)))
 
         # pronalazenje karakteristika na slici
-        #kp_o, des_o = orb.detectAndCompute(obj, None)
-        #if len(kp_o) == 0 or des_o == None: continue
+        kp_o, des_o = orb.detectAndCompute(obj, None)
+        if len(kp_o) == 0 or des_o == None: continue
 
-        #matches = bf.match(des_r, des_o)
+        matches = bf.match(des_r, des_o)
 
         # obelezavanje detektovanog znaka
-       # if (len(matches) >= MATCH_THRESHOLD):
-        cv2.rectangle(street, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        if (len(matches) >= MATCH_THRESHOLD):
+            cv2.rectangle(street, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            print 'Koordinate: {0}, '.format((x, y, w, h))
 
-        reg = kp.img_to_array(cv2.resize(street[y:y+h+1, x:x+w+1], (224, 224)))
-        reg = np.expand_dims(reg, axis=0)
-        reg = preprocess_input(reg)
-        preds = model.predict(reg)
         # decode the results into a list of tuples (class, description, probability)
         # (one such list for each sample in the batch)
-        print 'Koordinate: {0}, prepoznato: {1} '.format((x, y, w, h), decode_predictions(preds, top=3)[0])
+
 
 
     return street
