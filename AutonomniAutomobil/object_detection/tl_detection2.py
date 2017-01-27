e481e5f2e5ef58598245c00ecab0af130c8d057d
alien93 <nina.marjanovic@gmail.com>
2017-01-27 12:43:14 +0100

    tl_detection test cascade

---
+++ AutonomniAutomobil/object_detection/tl_detection2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+
+def detect(img):
+    tl_cascade = cv2.CascadeClassifier('tl_detection_cascade.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    traffic_lights = tl_cascade.detectMultiScale(gray, 1.3, 5)
+    print len(traffic_lights)
+    for (x,y,w,h) in traffic_lights:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    #cv2.imshow('img',img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
