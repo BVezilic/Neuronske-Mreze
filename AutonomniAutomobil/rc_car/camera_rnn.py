5fd2d9a92858076a96255e4b45b3e7f4cf637e2c
Nina <nina.marjanovic@gmail.com>
2017-02-24 14:08:40 +0100

    camera rnn

--- AutonomniAutomobil/rc_car/camera_rnn.py
+++ AutonomniAutomobil/rc_car/camera_rnn.py
@@ -14,7 +14,7 @@ import controller
 import object_detection.lane_detector as fl
 from ann.rnn import rnn
 from ann.rnn import simple_rnn as sr
-#from ann.cnn import cnn
+from ann.cnn import cnn
 from object_detection import tl_detection3 as tld
 from object_detection import sign_detection as sd
 import sklearn.preprocessing as sp
@@ -22,11 +22,12 @@ import time
 
 
 class Camera(object):
-    def __init__(self, host='192.168.0.102:8080'):
+    def __init__(self, host='192.168.0.104:8080'):
         self.host = host
         self.car_controller = controller.CarController()
         self.rnn = sr.create_model()
         self.rnn.load_weights("../ann/rnn/weights.h5")
+        self.cnn = cnn.load_model()
         self.detected_stop = False
 
     def stream(self):
@@ -43,17 +44,19 @@ class Camera(object):
                 img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), 1)
                 # detect traffic light
                 tl_distance, img_tl = tld.detect(img)
-                cv2.imshow("tl",img_tl)
+
                 # detect STOP sign
                 if self.detected_stop is False:
                     stop_distance = sd.detect(img)[0]
+
                 # detect vehicle
-                #is_vehicle = cnn.is_car(img.copy()[70:144, 20:156], cnn.get_model())
+                is_vehicle = cnn.is_car(img.copy()[70:144, 20:156], self.cnn)
+                print is_vehicle
 
                 if tl_distance != -1:
                     self.car_controller.control([0.0, 0.0, 0.0])
                     print "TRAFFIC LIGHT - RED!"
-                elif stop_distance != -1:
+                if stop_distance != -1:
                     print "STOP"
                     start_time = time.time()
                     self.detected_stop = True
@@ -61,20 +64,20 @@ class Camera(object):
                         self.car_controller.control([0.0, 0.0, 0.0])
                     stop_distance = -1
                     self.detected_stop = True
-                else:
-                    # detect vehicle
-                    #is_vehicle = cnn.is_car(img.copy()[70:144, 20:156], cnn.get_model())
+                if is_vehicle:
+                    print "VEHICLE DETECTED!"
+                    self.car_controller.control([0.0, 0.0, 0.0])
                     # detect lanes
-                    img, left_distance, right_distance,  left_line, right_line = fl.detect_lanes(img)
+                img, left_distance, right_distance,  left_line, right_line = fl.detect_lanes(img)
 
-                    print left_distance
-                    print right_distance
-                    # control
-                    data = [left_distance, right_distance]
-                    output = self.rnn.predict(np.expand_dims(np.array([data]),axis=0))
-                    print output
-                    cv2.imshow(hoststr, img)
-                    self.car_controller.control(output[0][0])
+                #print left_distance
+                #print right_distance
+                # control
+                data = [left_distance, right_distance]
+                output = self.rnn.predict(np.expand_dims(np.array([data]),axis=0))
+                #print output
+                cv2.imshow(hoststr, img)
+                self.car_controller.control(output[0][0])
                 if cv2.waitKey(1) == 27:
                     exit(0)
 
